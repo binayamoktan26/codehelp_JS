a077bedbd56ab8e22204d4ceabd4cf1211a4706b
nawang tashi lama <binayamoktan26@gmail.com>
2025-04-09 22:41:39 +0545

    call stack

--- index.js
+++ index.js
@@ -453,3 +453,30 @@
 // let sayMyName = function (finalName) {
 //   console.log(finalName);
 // };
+
+// Function call stack
+// function greetMe(greet, fullName) {
+//   console.log("hello", fullName);
+//   greet();
+// }
+// let greet = function () {
+//   console.log("hello world");
+// };
+// greetMe(greet, "bianaya moktan");
+// greet();
+
+//
+// const arr = [
+//   function (a, b) {
+//     return a + b;
+//   },
+//   function (a, b) {
+//     return a - b;
+//   },
+//   function (a, b) {
+//     return a * b;
+//   },
+// ];
+// let first = arr[1];
+// let ans = first(2, 8);
+// console.log(ans);
